22cb74dc2e882ee460c5414aebb870d81b1231a4
maksimphono <trukhinmaksimof@mail.ru>
2025-07-17 03:48:48 +0800

    feat: created logic to keep sequence of entries as well as entries in stack

--- src/recursive_scan.cpp
+++ src/recursive_scan.cpp
@@ -1,27 +1,67 @@
 #include <iostream>
 #include <stack>
+#include <vector>
 #include <filesystem>
 using namespace std;
 
 namespace fs = filesystem;
 
 namespace recursive_scan_ns {
+    typedef struct {
+        uint8_t depth;
+        string name;
+    } PlantUMLEntry;
+
+    PlantUMLEntry* createPlantUMLEntry(uint8_t depth, string name){
+        PlantUMLEntry* created = new PlantUMLEntry;
+        created->depth = depth;
+        created->name = name;
+
+        return created;
+    }
+
+    vector<PlantUMLEntry> plantUML_entries = vector<PlantUMLEntry>(); // array that conatins entries of plantUML strings, these will be directly printed to a file, that will be used by plantUML engine
+
     void scan(string& root_path) {
         const fs::path path = fs::path(root_path);
 
+        plantUML_entries.push_back(*createPlantUMLEntry(0, root_path));
         try {
-            for (const auto& entry : fs::recursive_directory_iterator(path)) {
-                const fs::path currentPath = entry.path();
+            stack<int> entries_stack = stack<int>(); // stack, that will keep track of which directory I'm currently in, each element is an index of that directory in the 'plantUML_entries'
+            entries_stack.push(0); // first element in the stack is root
+
+            for (auto entry = fs::recursive_directory_iterator(path); entry != fs::recursive_directory_iterator(); entry++) {
+                const fs::path currentPath = entry->path();
+
+                uint depth = entry.depth() + 1; // + 1 because root has depth 0
+
+                if (depth > entries_stack.size()) {
+                    // entered a directory
+                    cout << "Entering directory " << plantUML_entries[plantUML_entries.size() - 1].name << "\n";
+                    entries_stack.push(plantUML_entries.size() - 1); // record pointer to the parent directory (so the last element in the sequence)
+                } else while (depth < entries_stack.size()) {
+                    //for (const auto& item : plantUML_entries) {
+                    //    cout << item.name << ", ";
+                    //}
+                    cout << "Exeting directory " << plantUML_entries.at(entries_stack.top()).name << "\n";
+                    entries_stack.pop();
+                }
+                // write current element to the sequence
+                PlantUMLEntry* created_entry = createPlantUMLEntry(depth, currentPath.filename().string());
+                plantUML_entries.push_back(*created_entry);
+
+                if (entries_stack.size()) {
+                    cout << plantUML_entries[entries_stack.size() - 1].name << " " << entries_stack.size() << endl;
+                }
 
                 if (fs::is_directory(currentPath)) { // if it's a directory
-                    cout << "[DIR] " << currentPath.filename().string() << endl;
+                    cout << "[DIR] " << currentPath.filename().string() << " " << entry.depth() << endl;
                 }
                 else if (fs::is_regular_file(currentPath)) { // if it's a file
-                    cout << "[FILE] " << currentPath.filename().string() << endl;
+                    cout << "[FILE] " << currentPath.filename().string() << " " << entry.depth() << endl;
                 } else {
-                    cout << "[OBJ] " << currentPath.string() << endl;
+                    cout << "[OBJ] " << currentPath.string() << " " << entry.depth() << endl;
                 }
-
             }
         } catch (const fs::filesystem_error& e) {
             cerr << "Filesystem error: " << e.what() << endl;
