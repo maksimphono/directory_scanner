c0ddb8ff1097632d4ca3a7833bf562e452728d8b
maksimphono <trukhinmaksimof@mail.ru>
2025-07-17 11:13:54 +0800

    feat: added types of entries (dir, file, symlink...)

--- src/recursive_scan.cpp
+++ src/recursive_scan.cpp
@@ -7,15 +7,19 @@ using namespace std;
 namespace fs = filesystem;
 
 namespace recursive_scan_ns {
+    typedef enum{DIR, FILE, SYMLINK, FIFO} EntryType;
+
     typedef struct {
         uint8_t depth;
+        EntryType type;
         string name;
     } PlantUMLEntry;
 
-    PlantUMLEntry* createPlantUMLEntry(uint8_t depth, string name){
+    PlantUMLEntry* createPlantUMLEntry(uint8_t depth, string name, EntryType type = FILE){
         PlantUMLEntry* created = new PlantUMLEntry;
         created->depth = depth;
         created->name = name;
+        created->type = type;
 
         return created;
     }
@@ -25,7 +29,7 @@ namespace recursive_scan_ns {
     vector<PlantUMLEntry>* scan(string& root_path) {
         const fs::path path = fs::path(root_path);
 
-        plantUML_entries.push_back(*createPlantUMLEntry(0, path.filename())); // first element in the stack is root
+        plantUML_entries.push_back(*createPlantUMLEntry(0, path.filename(), DIR)); // first element in the stack is root
         try {
             stack<int> entries_stack = stack<int>(); // stack, that will keep track of which directory I'm currently in, each element is an index of that directory in the 'plantUML_entries'
             entries_stack.push(0); // pushing root element
@@ -43,15 +47,22 @@ namespace recursive_scan_ns {
                     cout << "Exeting directory " << plantUML_entries.at(entries_stack.top()).name << "\n";
                     entries_stack.pop();
                 }
-                // write current element to the sequence
-                PlantUMLEntry* created_entry = createPlantUMLEntry(depth, currentPath.filename().string());
+                // write current element to the sequence (FILE initialy and by default)
+                PlantUMLEntry* created_entry = createPlantUMLEntry(depth, currentPath.filename().string(), FILE);
+
+                if (fs::is_directory(currentPath)) { // if it's a directory
+                    created_entry->type = DIR;
+                } else if (fs::is_symlink(currentPath)) {
+                    created_entry->type = SYMLINK;
+                }
+
                 plantUML_entries.push_back(*created_entry);
 
                 // print the top element in the stack
                 if (entries_stack.size()) {
                     cout << plantUML_entries[entries_stack.size() - 1].name << " " << entries_stack.size() << endl;
                 }
-
+/*
                 if (fs::is_directory(currentPath)) { // if it's a directory
                     cout << "[DIR] " << currentPath.filename().string() << " " << entry.depth() << endl;
                 }
@@ -60,11 +71,11 @@ namespace recursive_scan_ns {
                 } else {
                     cout << "[OBJ] " << currentPath.string() << " " << entry.depth() << endl;
                 }
-
-                cout << "Got:\n";
-                for (const auto& elem : plantUML_entries) {
-                    cout << elem.name << " ";
-                }
+*/
+            }
+            cout << "Got:\n";
+            for (const auto& elem : plantUML_entries) {
+                cout << elem.name << " " << elem.type << " ";
             }
             return &plantUML_entries;
 
