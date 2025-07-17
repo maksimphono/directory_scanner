838a1917605a87230762bf459897e0ebe8aa7451
maksimphono <trukhinmaksimof@mail.ru>
2025-07-18 02:55:44 +0800

    style: code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,9 @@
 #include <cstdint>
 
 #include "cli_arguments.cpp"
+#include "recursive_scan.cpp"
 
 using namespace cli_arguments_ns;
-
-#include "recursive_scan.cpp"
 using namespace std;
 
 /*
@@ -58,17 +57,6 @@ struct {
     string* output_path;
 } cli_arguments;
 
-const auto get_cli_arguments(int n_args, const char** v_args) {
-    string* raw_arguments[n_args - 1];
-
-    for (uint8_t i = 0; i < n_args - 1; i++) {
-        raw_arguments[i] = new string(v_args[i + 1]);
-    }
-
-    cli_arguments.path = raw_arguments[0]; // fisrt argument must be a path to directory to scan
-
-    return &cli_arguments;
-}
 
 int main(int n_args, const char** v_args) {
     try {
